1fb4123aec627c7ed25985214e0d0a7e988e6b52
franswjy403 <73325365+franswjy403@users.noreply.github.com>
2021-03-26 12:20:51 +0700

    Ternyata belum fix, ini fix harusnya

--- soal3/soal3a.sh
+++ soal3/soal3a.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-> /home/frans0416/Documents/sisopE/soal3/Foto.log
-first_ind=0;
+loc=$(pwd)
+> "$loc"/Foto.log
 for ((i=1; i<=23; i=i+1))
     do
     if [ $i -lt 10 ]
-        then wget -a /home/frans0416/Documents/sisopE/soal3/Foto.log "https://loremflickr.com/320/240/kitten" -O /home/frans0416/Documents/sisopE/soal3/Koleksi_0"$i".jpeg
-    else wget -a /home/frans0416/Documents/sisopE/soal3/Foto.log "https://loremflickr.com/320/240/kitten" -O /home/frans0416/Documents/sisopE/soal3/Koleksi_"$i".jpeg
+        then wget -a "$loc"/Foto.log "https://loremflickr.com/320/240/kitten" -O "$loc"/Koleksi_0"$i".jpeg
+    else wget -a "$loc"/Foto.log "https://loremflickr.com/320/240/kitten" -O "$loc"/Koleksi_"$i".jpeg
     fi
 done
-rdfind -deleteduplicates true /home/frans0416/Documents/sisopE/soal3
+rdfind -deleteduplicates true "$loc"
 i=1
+cd "$loc"
 for f in *.jpeg
     do
     if [ $i -lt 10 ]
