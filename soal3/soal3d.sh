eab00a87af6438d7e476c3eb5ffafe9bd135c23e
frans0416 <franswijaya403@gmail.com>
2021-03-27 14:42:06 +0700

    update pengondisian substring

--- soal3/soal3d.sh
+++ soal3/soal3d.sh
@@ -4,10 +4,10 @@ SUB2="Kucing"
 password=$(date +%m%d%Y)
 for f in *; do
     if [ -d "$f" ]; then
-        if [[ "$f" == *"$SUB"* ]] 
+        if [[ "$f" == "$SUB"_* ]] 
         then
             zip -r -P "$password" Koleksi.zip "$f"
-        elif [[ "$f" == *"$SUB2"* ]] 
+        elif [[ "$f" == "$SUB2"_* ]] 
         then   
             zip -r -P "$password" Koleksi.zip "$f"
         fi
